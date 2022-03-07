ed32cd3ce42e64f04cb57d89359fb506bcb66f04
JaeUkPark <96907816+JaeUkPark@users.noreply.github.com>
2022-03-07 15:16:39 +0900

    Create GF_Multiplier.v

---
+++ GF_Multiplier.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module GF_Multiplier(
+    input [7:0] in1,
+    input [7:0] in2,
+    output [7:0] out
+    );
+    
+//Modulo polynomials    
+localparam modulo = 9'b100011011;
+
+/***********************************
+
+
+
+************************************/
+endmodule
