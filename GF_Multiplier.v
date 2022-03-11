af22fd41e5b9559c530515636f77cd62f3fb34ab
JaeUkPark <96907816+JaeUkPark@users.noreply.github.com>
2022-03-11 14:58:17 +0900

    Update GF_Multiplier.v

--- GF_Multiplier.v
+++ GF_Multiplier.v
@@ -5,53 +5,46 @@ module GF_Multiplier(
     input [7:0] in2,
     output [7:0] out
     );
-    
-//Modulo polynomials    
-localparam modulo = 8'b00011011; // 9'b100011011
-integer i;
-integer j;
-
-reg [8:0] tmp = 9'b0_0000_0000;
-wire [7:0] GF_out;
-reg [7:0] acc;
-
-always @(in1, in2)
-begin
-    acc = 8'b0000_0000;
-    for(i=7; i>=0; i=i-1) begin
-        if(in1[i] == 1'b1) begin
-            tmp[8] = 1'b0;
-            tmp[7:0] = in2;
-            for(j=1; j<=i; j=j+1) begin
-                tmp = tmp << 1;
-                #5;
-                if(tmp[8] == 1'b1) begin
-                    //#5;
-                    tmp[8] = 1'b0;
-                    tmp[7] = tmp[7]^modulo[7];
-                    tmp[6] = tmp[6]^modulo[6];
-                    tmp[5] = tmp[5]^modulo[5];
-                    tmp[4] = tmp[4]^modulo[4];
-                    tmp[3] = tmp[3]^modulo[3];
-                    tmp[2] = tmp[2]^modulo[2];
-                    tmp[1] = tmp[1]^modulo[1];
-                    tmp[0] = tmp[0]^modulo[0];
-                    //tmp[7:0] = GF_out;
-                end
-            end
-            acc[7] = acc[7]^tmp[7];
-            acc[6] = acc[6]^tmp[6];
-            acc[5] = acc[5]^tmp[5];
-            acc[4] = acc[4]^tmp[4];
-            acc[3] = acc[3]^tmp[3];
-            acc[2] = acc[2]^tmp[2];
-            acc[1] = acc[1]^tmp[1];
-            acc[0] = acc[0]^tmp[0];              
-        end
-    end
-end
-
-//GF_Adder GFA1(.in1(tmp[7:0]),.in2(modulo),.out(GF_out));
-assign out = acc;
+    assign out[7] = ((~in1[0])&in2[6])
+                    |((~in1[1])&in2[7])
+                    |(in1[0]&in2[7]&(~in2[6]))
+                    |(in1[1]&(~in2[7])&in2[6]);
+    assign out[6] = ((~in1[0])&in2[5])
+                    |((~in1[1])&in2[6])
+                    |(in1[0]&in2[6]&(~in2[5]))
+                    |(in1[1]&(~in2[6])&in2[5]);
+    assign out[5] = ((~in1[0])&in2[4])
+                    |((~in1[1])&in2[5])
+                    |(in1[1]&(~in2[5])&in2[4])
+                    |(in1[0]&in2[5]&(~in2[4]));
+    assign out[4] = ((~in1[1])&in2[4])
+                    |(in1[0]&in2[7]&in2[4]&in2[3])
+                    |((~in1[0])&in2[7]&(~in2[3]))
+                    |((~in1[0])&(~in2[7])&in2[3])
+                    |(in1[0]&(~in2[7])&in2[4]&(~in2[3]))
+                    |(in1[1]&in2[7]&(~in2[4])&(~in2[3]))
+                    |(in1[1]&(~in2[7])&(~in2[4])&in2[3]);
+    assign out[3] = ((~in1[1])&in2[3])
+                    |((~in1[0])&in2[7]&(~in2[2]))
+                    |(in1[0]&in2[7]&in2[3]&in2[2])
+                    |(in1[1]&in2[7]&(~in2[3])&(~in2[2]))
+                    |((~in1[0])&(~in2[7])&in2[2])
+                    |(in1[0]&(~in2[7])&in2[3]&(~in2[2]))
+                    |(in1[1]&(~in2[7])&(~in2[3])&in2[2]);
+    assign out[2] = ((~in1[0])&in2[1])
+                    |(in1[1]&(~in2[2])&in2[1])
+                    |((~in1[1])&in2[2])
+                    |(in1[0]&in2[2]&(~in2[1]));
+    assign out[1] = ((~in1[1])&in2[1])
+                    |((~in1[0])&(~in2[7])&in2[0])
+                    |(in1[0]&in2[7]&in2[1]&in2[0])
+                    |((~in1[0])&in2[7]&(~in2[0]))
+                    |(in1[0]&(~in2[7])&in2[1]&(~in2[0]))
+                    |(in1[1]&in2[7]&(~in2[1])&(~in2[0]))
+                    |(in1[1]&(~in2[7])&(~in2[1])&in2[0]);
+    assign out[0] = ((~in1[0])&in2[7])
+                    |(in1[1]&in2[7]&(~in2[0]))
+                    |((~in1[1])&in2[0])
+                    |(in1[0]&(~in2[7])&in2[0]);
 
 endmodule
