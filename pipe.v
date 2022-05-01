3883f77aab3c5624d72e8f3936ba0c84d7aa9969
movie5 <43196430+movie5@users.noreply.github.com>
2022-05-01 17:06:56 +0900

    Update pipe.v
    주석달기

--- pipe.v
+++ pipe.v
@@ -1,17 +1,17 @@
 `timescale 1ns / 1ps
 
-
+//register, 9번 돌아가기 위해
 module pipe(
-    input [127:0] in,
+    input [127:0] in,  //mux의 결과에 따라 pre-round의 결과나 round의 결과를 받는다
     input clk,
-    output [127:0] out
+    output [127:0] out // 다음 round의 input
     );
     
     reg tmp;
     
-    always @(posedge clk)
+    always @(posedge clk) //clk+
     begin
-        tmp <= in;
+        tmp <= in; 
     end
     
     assign out = tmp;
